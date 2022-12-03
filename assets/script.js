c686909d50d6e35b938000c097000b39268f26c4
kyleroff112 <kyleroff112@gmail.com>
2022-12-02 20:19:58 -0500

    whole bunch of javascript and html changes

--- assets/script.js
+++ assets/script.js
@@ -6,44 +6,50 @@ $('.autoplay').slick({
   });
           
 
-  var movie = document.getElementById("movie");
-var searchBtn = document.getElementById("search-btn");
-var wikiInfo = document.getElementById("wiki-info");
-function getWikiApi() {
-  //create variable to collect the user input
-  var term = document.getElementById("movie").value;
-  //generate the search url using the search term
-  searchUrl = "https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=" + term + "&gsrlimit=1&prop=pageimages|extracts&exchars=5000&exintro&explaintext&exlimit=max&format=json&origin=*";
-  //make a fetch call to that endpoint
-  fetch(searchUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      //create an array to store the data in
-      const resultArray = [];
-      //loop through every key in the pages object (will only be 1 as declared in the search url with &gsrlimit=1)
-      Object.keys(data.query.pages).forEach(key => {
-        //create variables for the wiki article title and extract
-        const title = data.query.pages[key].title;
-        const text = data.query.pages[key].extract;
-        //place both of these items into an object
-        const item = {
-          title: title,
-          text: text
-        };
-        //put that object into the results array
-        resultArray.push(item);
-      });
-      // update/display the text content of the wikipedia section
-      wikiInfo.textContent = resultArray[0].text;
-    })
-}
-searchBtn.addEventListener('click', getWikiApi);
-movie.addEventListener("keypress", function(event){
-    if(event.key === "Enter"){
-        event.preventDefault();
-        searchBtn.click();
-    }
-});
+  var searchButton = document.getElementById("searchButton");
+  var cardTitle = document.getElementById("movie-title-1");
+  var contentTitle = document.getElementById("movie-description-1");
+  var wikiInfo = document.getElementById("wiki-info");
+  var pictures = document.getElementById("poster")
+
+
+  function getApi(event) {
+      event.preventDefault()
+      var searchTerms = document.getElementById("searchTerms").value
+      var tmdbApi = "https://api.themoviedb.org/3/search/movie?api_key=e9edbb7e687ae04d372f978edb104c8b&query=" + searchTerms + "&format=json"
+      fetch(tmdbApi)
+          .then(function (response){
+              return response.json();
+          })
+          .then(function(data){
+              var horrorMovies = data.results.filter(function(result){
+                  if(result.genre_ids.includes(27)){
+                      return result
+                  }
+              })
+              cardTitle.textContent = horrorMovies[0].original_title;
+              contentTitle.textContent = horrorMovies[0].overview;
+             pictures.innerHTML = "<img src="+"https://image.tmdb.org/t/p/w500"+ horrorMovies[0].poster_path+">";
+
+      var wikiApi = "https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=" + horrorMovies[0].original_title + "&gsrlimit=1&prop=pageimages|extracts&exchars=5000&exintro&explaintext&exlimit=max&format=json&origin=*";
+            fetch(wikiApi)
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (data) {
+                console.log(data);
+                const resultArray = [];
+                Object.keys(data.query.pages).forEach(key => {
+                  const title = data.query.pages[key].title;
+                  const text = data.query.pages[key].extract;
+                  const item = {
+                    title: title,
+                    text: text
+                  };
+                  resultArray.push(item);
+                });
+                wikiInfo.textContent = resultArray[0].text;
+              })})
+  }  
+  
+  searchButton.addEventListener("click", getApi)
